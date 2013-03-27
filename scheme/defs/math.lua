1b44d3764a39f1054bdd192a5737cb64e5c72c8a
Konstantin Stepanov <me@kstep.me>
2013-03-27 18:01:33 +0300

    Add special math functions and constants

---
+++ scheme/defs/math.lua
@@ -0,0 +1,42 @@
+local genv = require("scheme.env")
+local math = math
+
+genv:_define {
+    -- Constants {{{
+    pi = math.pi,
+    e = math.exp(1),
+    -- }}}
+
+    ln = function (env, num)
+        return math.log(env:_eval(num))
+    end,
+    log = function (env, num, base)
+        return math.log(env:_eval(num), env:_eval(base))
+    end,
+    
+    -- Trigonometry {{{
+    sin = function (env, arg)
+        return math.sin(env:_eval(arg))
+    end,
+    asin = function (env, arg)
+        return math.asin(env:_eval(arg))
+    end,
+
+    cos = function (env, arg)
+        return math.cos(env:_eval(arg))
+    end,
+    acos = function (env, arg)
+        return math.acos(env:_eval(arg))
+    end,
+
+    tan = function (env, arg)
+        return math.tan(env:_eval(arg))
+    end,
+    atan = function (env, arg)
+        return math.atan(env:_eval(arg))
+    end,
+    atan2 = function (env, a, b)
+        return math.atan2(env:_eval(a), env:_eval(b))
+    end,
+    -- }}}
+}
