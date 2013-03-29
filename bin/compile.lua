0531d673ba4716cd56a0dd9c39bb65a169b198d7
Konstantin Stepanov <me@kstep.me>
2013-03-30 02:50:48 +0300

    Compiler utility accepts output file

--- bin/compile.lua
+++ bin/compile.lua
@@ -3,8 +3,16 @@
 local scheme = require("scheme")
 scheme.util = require("scheme.util")
 
-local code = scheme.compilefile(arg[1])
+local infile = arg[1]
+local outfile = arg[2]
 
-print([[#!/usr/bin/lua
+local code = scheme.compilefile(infile)
+
+local result = ([[#!/usr/bin/lua
 require("scheme").eval(]] .. scheme.util.lua_dump(code) .. [[)]])
 
+if infile and not outfile then
+    outfile = infile:gsub("[.]scm$", ".lscm")
+end
+
+io.output(outfile):write(result)
