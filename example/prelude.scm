34589c1be6d6da07bb12ec9fcaa80794693b7fe8
Konstantin Stepanov <me@kstep.me>
2013-03-31 13:53:50 +0300

    Add (filter) and (qsort) to prelude

--- example/prelude.scm
+++ example/prelude.scm
@@ -15,3 +15,23 @@
 (define (fold-right fn init lst)
   (if (null? lst) init
     (fn (car lst) (fold-right fn init (cdr lst)))))
+
+(define (filter fn lst)
+  (if (null? lst) lst
+    (if (fn (car lst))
+      (cons (car lst) (filter fn (cdr lst)))
+      (filter fn (cdr lst)))))
+
+(define (concat a b)
+  (if (null? a) b
+    (cons (car a) (concat (cdr a) b))))
+
+(define (qsort key lst)
+  (if (null? lst) lst
+    (let* ([pivot (key (car lst))]
+           [rest (cdr lst)]
+           [left (filter (lambda (a) (< (key a) pivot)) rest)]
+           [right (filter (lambda (a) (> (key a) pivot)) rest)])
+      (concat (qsort key left) (cons (car lst) (qsort key right))))))
+
+(define (id x) x)
